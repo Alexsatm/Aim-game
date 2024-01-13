a44626752328ef7ba90da07a3ec5b729768e8ee6
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-13 21:54:03 +0300

    add js var

---
+++ app.js
@@ -0,0 +1,8 @@
+const startBtn = document.querySelector('#start');
+const screens = document.querySelectorAll('.screen');
+const timeList = document.querySelector('#time-list');
+const timeEl = document.querySelector('#time');
+const board = document.querySelector('#board');
+
+let time = 0;
+let score = 0;
