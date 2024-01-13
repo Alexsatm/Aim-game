d113789891ec47e4c1e4840e72b8eb32f5ff6548
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-13 21:57:24 +0300

    add function createRandomCircle

--- app.js
+++ app.js
@@ -54,3 +54,23 @@ function finishGame(){
     timeEl.parentNode.classList.add('hide')
     board.innerHTML = `<h1>Счет: <span class="primary">${score}</span></h1>`
 }
+
+function createRandomCircle(){
+    const circle = document.createElement('div')
+    const size = getRandomNumber(10, 60)
+    const {width, height} = board.getBoundingClientRect()
+    const x = getRandomNumber(0, width - size)
+    const y = getRandomNumber(0, height - size)
+    // color
+    const color = getRandomColor()
+    circle.style.background = getRandomColor()
+    circle.style.background = `0 0 2px ${color}, 0 0 10px ${color}`
+
+    circle.classList.add('circle')
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${y}px`
+    circle.style.left = `${x}px`
+
+    board.append(circle)
+}
